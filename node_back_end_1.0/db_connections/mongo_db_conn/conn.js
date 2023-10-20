c556d8d0751f85f8065f55341a87492d5b094ddc
Govindgiri-openseLabs <140603888+Govindgiri-openseLabs@users.noreply.github.com>
2023-10-20 23:32:44 +0530

    Update conn.js

--- node_back_end_1.0/db_connections/mongo_db_conn/conn.js
+++ node_back_end_1.0/db_connections/mongo_db_conn/conn.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 const MONGO_URL=process.env.mongo_url;
 
 const conn=async()=>{
-    await mongoose.connect(MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    await mongoose.connect("mongodb+srv://govindgirigoswami1501:Govind1501@cluster0.bzdnedh.mongodb.net/my_db",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("mongodb connected");
 }).catch((error)=>{
     console.log("db connection failed",error);
